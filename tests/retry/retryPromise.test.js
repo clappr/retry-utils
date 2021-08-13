88c4e1313f8ac7b383bd9ac8333a05da889632f5
Leticia Fernandes <leticiacf.fernandes@gmail.com>
2021-08-13 14:43:23 -0300

    test(retry promise): test type of action parameter

--- tests/retry/retryPromise.test.js
+++ tests/retry/retryPromise.test.js
@@ -4,12 +4,17 @@ import retryPromise from '../../lib/j/retryPromise'
 describe('Retry Promise Tests', () => {
   test('should reject promise if action parameter is null', () => {
     let invalidAction = retryPromise({ action: null })
-    return expect(invalidAction).rejects.toEqual('Action must be defined')
+    return expect(invalidAction).rejects.toEqual('Action must be defined and a function')
   })
 
   test('should reject promise if action parameter is empty', () => {
     let invalidAction = retryPromise({})
     return expect(invalidAction).rejects.toEqual('Action must be defined')
+  })
+
+  test('should reject promise if action parameter is not a function', () => {
+    let invalidAction = retryPromise({ action: Promise.resolve() })
+    return expect(invalidAction).rejects.toEqual('Action must be defined and a function')
   })
 
   test('should reject promise if a non mapped parameter is passed', () => {
