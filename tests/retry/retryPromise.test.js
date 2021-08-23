5af983bb53119272254d1c4cfbffa91500527577
Leticia Fernandes <leticiacf.fernandes@gmail.com>
2021-08-23 16:31:06 -0300

    test(retry promise): fix tests

--- tests/retry/retryPromise.test.js
+++ tests/retry/retryPromise.test.js
@@ -4,22 +4,22 @@ import retryPromise from '../../lib/j/retryPromise'
 describe('Retry Promise Tests', () => {
   test('should reject promise if action parameter is null', () => {
     let invalidAction = retryPromise({ action: null })
-    return expect(invalidAction).rejects.toEqual('Action must be defined and a function')
+    return expect(invalidAction).rejects.toEqual({ retryError: 'Action must be defined and a function', retryHistory: []})
   })
 
   test('should reject promise if action parameter is empty', () => {
     let invalidAction = retryPromise({})
-    return expect(invalidAction).rejects.toEqual('Action must be defined and a function')
+    return expect(invalidAction).rejects.toEqual({ retryError: 'Action must be defined and a function', retryHistory: []})
   })
 
   test('should reject promise if action parameter is not a function', () => {
     let invalidAction = retryPromise({ action: Promise.resolve() })
-    return expect(invalidAction).rejects.toEqual('Action must be defined and a function')
+    return expect(invalidAction).rejects.toEqual({ retryError: 'Action must be defined and a function', retryHistory: []})
   })
 
   test('should reject promise if a non mapped parameter is passed', () => {
     let invalidAction = retryPromise({ nonExistent: 'x', dontKnow: 123 })
-    return expect(invalidAction).rejects.toEqual('Action must be defined and a function')
+    return expect(invalidAction).rejects.toEqual({ retryError: 'Action must be defined and a function', retryHistory: []})
   })
 
   test('should return rejected promise if all attempts fail', () => {
